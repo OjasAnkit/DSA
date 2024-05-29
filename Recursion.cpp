b17f15239ca92d38a3afe03c0cbd75fa593f5209
Ojas Ankit <ojasankit@gmail.com>
2024-05-29 23:31:45 +0530

    Solved recursion hw problems

--- Recursion.cpp
+++ Recursion.cpp
@@ -34,6 +34,27 @@ int checkForOverlappingA(vector<char> v, int start)
     return count;
 }
 
+int aaa_alt_non_overlapping(vector<char> v, int start)
+{
+    // base case
+    if (start >= v.size() - 2)
+        return 0;
+
+    // faith
+    // temp = aaa_alt(v, start + 1);
+    // cout << temp << endl;
+
+    // smaller problem
+    if (v[start] == 'a' && v[start + 1] == 'a' && v[start + 2] == 'a')
+    {
+        int temp = aaa_alt_non_overlapping(v, start + 3);
+        return ++temp;
+    }
+    else
+        return aaa_alt_non_overlapping(v, start + 1);
+    ;
+}
+
 int checkForA(vector<char> v, int start)
 {
     int count = 0;
@@ -52,20 +73,51 @@ int checkForA(vector<char> v, int start)
         }
     }
     else
-        count += checkForOverlappingA(v, start +);
+        // count += checkForOverlappingA(v, start +);
 
-    if (start + 1 < v.size() - 3)
+        if (start + 1 < v.size() - 3)
 
-        return count;
+            return count;
+}
+
+bool checkSort(vector<int> v, int start)
+{
+    if (start >= v.size() - 2)
+        return (v[start] <= v[start + 1]);
+    if (checkSort(v, start + 1))
+        return (v[start] <= v[start + 1]);
+    else
+        return false;
+}
+
+int firstIndex(vector<int> v, int k, int start)
+{
+    if (start == v.size() - 1)
+    {
+        if (v[start] == k)
+            return start;
+        else
+            return -1;
+    }
+
+    int temp = firstIndex(v, k, start + 1);
+
+    if (v[start] == k)
+        return start;
+    else
+        return temp;
 }
 
 int main()
 {
-    vector<char> v = {'a', 'a', 'a', 'a', 'a', 'a', 'a'};
+    vector<char> v = {'a', 'a', 'b', 'a', 'a', 'a', 'c', 'c', 'a', 'a', 'a', 'b', 'c', 'a', 'a'};
+    vector<int> v1 = {1, 0, 7, 8, 9, 4, 5, 6, 7, 7, 7, 8, 9};
+    int k = 6;
+    // cout << "Number of overlapping aaa in vector is : " << aaa_alt_non_overlapping(v, 0)<<endl;
 
-    cout << "Number of overlapping aaa in vector is : " << checkForOverlappingA(v, 0) << endl;
+    cout << "First Index of " << k << " is: " << firstIndex(v1, k, 0);
 
-    cout << "Number of non-overlapping aaa in vector is : " << checkForA(v, 0) << endl;
+    // cout << "Number of non-overlapping aaa in vector is : " << checkForA(v, 0) << endl;
 
     return 0;
 }
